7067924844868d961961a94ff2e33678914a59b7
Chris Richard <chris.richard@ratehub.ca>
2018-10-16 11:33:47 -0400

    Remove Express and Mongoose.

--- Dockerfile
+++ Dockerfile
@@ -8,5 +8,3 @@ ADD node_modules /software/express-server/dist/node_modules
 WORKDIR /software/express-server/dist
 
 CMD ["node", "server.js"]
-
-MAINTAINER tomyitav@gmail.com
--- server.js
+++ server.js
@@ -1,11 +1,5 @@
-import express from "express";
-import bodyParser from "body-parser";
-import cors from "cors";
 import logger from './core/logger/app-logger'
-import morgan from 'morgan'
 import config from './core/config/config.dev'
-import cars from './routes/cars.route'
-import connectToDb from './db/connect'
 
 const port = config.serverPort;
 logger.stream = {
@@ -14,21 +8,3 @@ logger.stream = {
     }
 };
 
-connectToDb();
-
-const app = express();
-app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(morgan("dev", { "stream": logger.stream }));
-
-app.use('/cars', cars);
-
-//Index route
-app.get('/', (req, res) => {
-    res.send('Invalid endpoint!');
-});
-
-app.listen(port, () => {
-    logger.info('server started - ', port);
-});
